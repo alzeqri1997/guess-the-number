87823dd80c2240742d6d3aab87d8f944f429e4f3
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-08-27 19:28:29 +0300

    check if player wins or loses

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ const displayMessage = function (message) {
     document.querySelector('.message').textContent = message;
 };
 
+document.querySelector('.check').addEventListener('.click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        displayMessage('NO NUMBER')
+    } else if (guess === secretNumber) {
+        displayMessage('CORRECT NUMBER');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('.body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    } else if (guess !== secretNumber){
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? 'Too high' : 'Too low!')
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage("you lost the game");
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
